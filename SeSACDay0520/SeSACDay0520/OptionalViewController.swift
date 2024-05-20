1181fa4b26463870c8e3b692c4e48f473a27475a
Hminchae <hminchae@gmail.com>
2024-05-20 14:22:08 +0900

    Any vs UIButton
    Any를 사용하는 예 : 같은 액션을 다중 버튼 & 제스처가 담당할 때
    ex) 키보드 내리기

---
+++ SeSACDay0520/SeSACDay0520/OptionalViewController.swift
@@ -0,0 +1,28 @@
+//
+//  OptionalViewController.swift
+//  SeSACDay0520
+//
+//  Created by 황민채 on 5/20/24.
+//
+
+import UIKit
+
+class OptionalViewController: UIViewController {
+    
+    @IBOutlet weak var blueButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    // TODO: 키보드 내리기
+    // 1. 바탕 탭제스처
+    // 2. return
+    // 3. 버튼클릭 시
+    
+    // Any를 사용하는 예
+    // 같은 액션을 다중 버튼 & 제스처가 담당할 때
+    @IBAction func keyboardDismiss(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+}
