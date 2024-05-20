1e73f64795b02e86e48a43b43db2091680ba8b41
Hminchae <hminchae@gmail.com>
2024-05-21 01:36:43 +0900

    Refactor : LoginViewController 반복되는 뷰 로직 리팩토링
    - 함수 활용
    - IBOutletCollection 활용

--- Netflix/Netflix/LoginViewController.swift
+++ Netflix/Netflix/LoginViewController.swift
@@ -10,14 +10,8 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var loginTitleLabel: UILabel!
-    
-
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var nickNameTextField: UITextField!
-    @IBOutlet weak var locationTextField: UITextField!
-    @IBOutlet weak var recomendCodeTextField: UITextField!
-    
+     
+    @IBOutlet var textFieldCollection: [UITextField]!
     @IBOutlet weak var signInButton: UIButton!
     
     @IBOutlet weak var addInfoLabel: UILabel!
@@ -29,46 +23,16 @@ class LoginViewController: UIViewController {
         loginTitleLabel.font = .systemFont(ofSize: 30, weight: .heavy)
         loginTitleLabel.textColor = .red
         
-        emailTextField.placeholder = "이메일 주소 또는 전화번호"
-        emailTextField.backgroundColor = .gray
-        emailTextField.textColor = .white
-        emailTextField.tintColor = .white
-        emailTextField.font = .systemFont(ofSize: 16, weight: .medium)
-        emailTextField.cornerRadius = 5
-        emailTextField.textAlignment = .center
+        designLoginTextField(textFieldCollection[0], placeholder: "이메일 주소 또는 전화번호")
         
-        passwordTextField.placeholder = "비밀번호"
-        passwordTextField.backgroundColor = .gray
-        passwordTextField.textColor = .white
-        passwordTextField.tintColor = .white
-        passwordTextField.font = .systemFont(ofSize: 16, weight: .medium)
-        passwordTextField.cornerRadius = 5
-        passwordTextField.textAlignment = .center
-        passwordTextField.isSecureTextEntry = true
+        designLoginTextField(textFieldCollection[1], placeholder: "비밀번호")
+        textFieldCollection[1].isSecureTextEntry = true
         
-        nickNameTextField.placeholder = "닉네임"
-        nickNameTextField.backgroundColor = .gray
-        nickNameTextField.textColor = .white
-        nickNameTextField.tintColor = .white
-        nickNameTextField.font = .systemFont(ofSize: 16, weight: .medium)
-        nickNameTextField.cornerRadius = 5
-        nickNameTextField.textAlignment = .center
+        designLoginTextField(textFieldCollection[2], placeholder: "닉네임")
         
-        locationTextField.placeholder = "위치"
-        locationTextField.backgroundColor = .gray
-        locationTextField.textColor = .white
-        locationTextField.tintColor = .white
-        locationTextField.font = .systemFont(ofSize: 16, weight: .medium)
-        locationTextField.cornerRadius = 5
-        locationTextField.textAlignment = .center
+        designLoginTextField(textFieldCollection[3], placeholder: "위치")
         
-        recomendCodeTextField.placeholder = "추천 코드 입력"
-        recomendCodeTextField.backgroundColor = .gray
-        recomendCodeTextField.textColor = .white
-        recomendCodeTextField.tintColor = .white
-        recomendCodeTextField.font = .systemFont(ofSize: 16, weight: .medium)
-        recomendCodeTextField.cornerRadius = 5
-        recomendCodeTextField.textAlignment = .center
+        designLoginTextField(textFieldCollection[4], placeholder: "추천 코드 입력")
         
         signInButton.setTitle("회원가입", for: .normal)
         signInButton.setTitleColor(.black, for: .normal)
@@ -81,11 +45,22 @@ class LoginViewController: UIViewController {
         addInfoSwitch.onTintColor = .red
         
     }
+    
+    func designLoginTextField(_ textFieldName : UITextField,
+                              placeholder: String
+    ) {
+        textFieldName.placeholder = placeholder
+        textFieldName.backgroundColor = .gray
+        textFieldName.textColor = .white
+        textFieldName.tintColor = .white
+        textFieldName.font = .systemFont(ofSize: 16, weight: .medium)
+        textFieldName.cornerRadius = 5
+        textFieldName.textAlignment = .center
+    }
+    
     @IBAction func signInAction(_ sender: UIButton) {
-        emailTextField.endEditing(true)
-        passwordTextField.endEditing(true)
-        nickNameTextField.endEditing(true)
-        locationTextField.endEditing(true)
-        recomendCodeTextField.endEditing(true)
+        for i in 0...4 {
+            textFieldCollection[i].endEditing(true)
+        }
     }
 }
