99812f0430c05e888cd7f686d5677e91a155d303
Hminchae <hminchae@gmail.com>
2024-05-24 02:25:45 +0900

    Day 8 Assignment : case 2 구현
    - case 1 실행 시 안 보이는 오류 수정
    - case 2 구현
      - section Title 추가
      - divider 추가
      - 재사용큐 내부 코드 구현

--- TableView/TableView/Case2TableViewController.swift
+++ TableView/TableView/Case2TableViewController.swift
@@ -8,82 +8,85 @@
 import UIKit
 
 class Case2TableViewController: UITableViewController {
-
+    
+    let all = ["공지사항", "실험실", "버전 정보"]
+    let personal = ["개인/보안", "알림", "채팅", "멀티프로필"]
+    let etc = ["고객센터/도움말"]
+    
+    @IBOutlet weak var allGroupTitle: UILabel!
     override func viewDidLoad() {
+        print(#function)
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        tableView.separatorStyle = .singleLine
+        tableView.separatorColor = .darkGray
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        print(#function)
+        var result: Int = 0
+        
+        if section == 0 {
+            result =  all.count
+        } else if section == 1 {
+            result =  personal.count
+        } else if section == 2 {
+            result =  etc.count
+        }
+        
+        return result
     }
-
-    /*
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 3
+    }
+    
+    // 2. 셀의 높이(약간 필수 권장...)
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        print(#function)
+        return 40
+    }
+    
+    // 3. 셀의 디자인 및 데이터 처리
+    // ex. 카카오톡 친구 이름. 프로필 사진. 상태 메시지 등
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        print(#function, indexPath)
+        // 3-1. 어떤 셀을 쓸 지 결정
+        // 재사용 메커니즘
+        let cell = tableView.dequeueReusableCell(withIdentifier: "case2")!
+        
+        if indexPath.section == 0 {
+            cell.textLabel?.text = all[indexPath.row]
+            cell.textLabel?.textColor = .label
+            cell.textLabel?.font = .systemFont(ofSize: 13)
+            cell.separatorInset.bottom = .greatestFiniteMagnitude
 
-        // Configure the cell...
-
+        } else if indexPath.section == 1 {
+            cell.textLabel?.text = personal[indexPath.row]
+            cell.textLabel?.textColor = .label
+            cell.textLabel?.font = .systemFont(ofSize: 13)
+        } else {
+            cell.textLabel?.text = etc[indexPath.row]
+            cell.textLabel?.textColor = .label
+            cell.textLabel?.font = .systemFont(ofSize: 13)
+        }
+    
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        var result: String?
+        
+        if section == 0 {
+            result =  "전체 설정"
+        } else if section == 1 {
+            result =  "개인 설정"
+        } else if section == 2 {
+            result =  "기타"
+        }
+        
+        return result
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
