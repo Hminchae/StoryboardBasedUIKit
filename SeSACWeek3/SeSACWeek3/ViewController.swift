ff6683ae3a49ab46bd382d07bad36afd1fa38246
Hminchae <hminchae@gmail.com>
2024-05-28 10:01:16 +0900

    Day 11 Class : 시드 파일 생성 및 구조 정리

---
+++ SeSACWeek3/SeSACWeek3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSACWeek3
+//
+//  Created by 황민채 on 5/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
