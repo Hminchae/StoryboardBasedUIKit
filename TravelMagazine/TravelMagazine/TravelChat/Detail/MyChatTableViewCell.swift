23ba260e7065c4b03038eef22182a876a2be4059
Hminchae <hminchae@gmail.com>
2024-06-01 15:49:35 +0900

    Day 14 Assignment : 채팅 디테일 뷰 xib 파일 오토레이아웃 적용

---
+++ TravelMagazine/TravelMagazine/TravelChat/Detail/MyChatTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyChatTableViewCell.swift
+//  TravelMagazine
+//
+//  Created by 황민채 on 6/1/24.
+//
+
+import UIKit
+
+class MyChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
