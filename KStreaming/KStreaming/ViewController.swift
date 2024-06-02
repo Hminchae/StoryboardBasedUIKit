e72a1a6d1630355ee9855ba9942b2e809ce5892e
Hminchae <hminchae@gmail.com>
2024-06-02 22:26:03 +0900

    K사 영상/스트리밍 앱 : 시드파일 생성

---
+++ KStreaming/KStreaming/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KStreaming
+//
+//  Created by 황민채 on 6/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
